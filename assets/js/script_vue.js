f6091ef6e0648f60069ff4390e473b099e251503
shubhamgpatel <shubhampatelpra96@gmail.com>
2025-05-20 15:18:55 -0400

    Optimizing Code with Vue Js Components

--- assets/js/script_vue.js
+++ assets/js/script_vue.js
@@ -1,67 +1,65 @@
 $(document).ready(function () {
     $('.fancybox').fancybox({
-        helpers: {
-            title: {
-                type: 'inside'
-            }
+      helpers: {
+        title: {
+          type: 'inside'
         }
-
+      }
     });
-}); // ready
+    Vue.component('section-title', {
+        props: ['title'],
+        template: `
+          <div class="section-title text-center mb-60">
+            <h2 class="mb-20 white-color">{{ title }}</h2>
+            <div class="horizontal-line">
+              <div class="top"></div>
+              <div class="bottom"></div>
+            </div>
+          </div>
+        `
+      });
+      Vue.component('experience-section', {
+        props: ['items'],
+        template: `
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 mobile-mb-30" v-for="(value, index) in items" :key="index">
+              <div class="left">
+                <div class="single-experi mb-50">
+                  <div class="left-text floatleft relative">
+                    <div class="dashed-line"></div>
+                    <h4 class="montserrat weight-medium mb-5 capitalize light-color">{{ value.company }}</h4>
+                    <p class="montserrat weight-medium">{{ value.year }}</p>
+                  </div>
+                  <div class="right-text">
+                    <i class="zmdi zmdi-check-circle"></i>
+                    <h3 class="capitalize font-20 light-color">{{ value.role }}</h3>
+                    <p>{{ value.desc }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+      });
 
-//    $( '[data-fancybox="images"]' ).fancybox({
-//     infobar : false,
-//     caption : function( instance, item ) {
-//       var caption = $(this).data('caption') || '';
-
-//       return ( caption.length ? caption + '<br />' : '' ) + 'Image <span data-fancybox-index></span> of <span data-fancybox-count></span>';
-//     }
-//   });
-
-
-const url = './assets/data.json';
-$.getJSON(url).then(data => {
-    var services = new Vue({
-        el: '#services',
-        data: { services: data.services },
-    })
-    console.log(data.projects);
-    var experience = new Vue({
-        el: '#experience',
-        data: { experience: data.experience },
-    })
-    var skills = new Vue({
-        el: '#skills',
-        data: { skills: data.skills },
-    })
-    var projects = new Vue({
-        el: '#projects',
-        data: { projects: data.projects },
-    })
-    var education = new Vue({
-        el: '#education',
-        data: { education: data.education },
-    })
-    var certifications = new Vue({
-        el: '#certifications',
-        data: { certifications: data.certifications },
-    })
-
-    var location = new Vue({
-        el: '#location',
-        data: { location: data.location },
-    })
-    var email = new Vue({
-        el: '#email',
-        data: { email: data.email },
-    })
-    var contact = new Vue({
-        el: '#contact',
-        data: { contact: data.contact },
-    })
-
-
-})
-//created() -> calls before the DOM is rendered
-//mounted() -> calls after the DOM is rendered
-//Computed()
+    const url = './assets/data.json';
+  
+    $.getJSON(url).then(data => {
+      new Vue({
+        el: '#app', // Make sure all your Vue elements are inside this container in HTML
+        data: {
+          services: data.services,
+          experience: data.experience,
+          skills: data.skills,
+          projects: data.projects,
+          education: data.education,
+          certifications: data.certifications,
+          contact: data.contact,
+          email: data.email,
+          location: data.location,
+          contactInfo: data.contactInfo
+        }
+      });
+    });
+  });
+  
